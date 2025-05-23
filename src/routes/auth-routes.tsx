b5e2f12c9579129cfd35d333e4bb2bab16059479
LuizGarbini <garbasneto@gmail.com>
2025-05-23 00:22:54 -0300

    Criando o AuthLayout

--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
-
+import { AuthLayout } from "../components/AuthLayout";
 import { Signin } from "../pages/Signin";
 
 export function AuthRoutes() {
 	return (
 		<Routes>
-			<Route path="/" element={<Signin />} />
+			<Route path="/" element={<AuthLayout />}>
+				<Route path="/" element={<Signin />} />
+			</Route>
 		</Routes>
 	);
 }
