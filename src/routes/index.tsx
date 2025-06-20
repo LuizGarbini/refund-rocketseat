4db31b274df07256fd17bceb52badef5b2eb058b
LuizGarbini <garbasneto@gmail.com>
2025-06-20 15:42:50 -0300

    Componente de Loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,19 @@
 import { BrowserRouter } from "react-router";
+
+import { Loading } from "../components/Loading";
 //import { EmployeeRoutes } from "./EmployeeRoutes";
-//import { AuthRoutes } from "./AuthRoutes";
-import { ManagerRoutes } from "./ManagerRoutes";
+import { AuthRoutes } from "./AuthRoutes";
+//import { ManagerRoutes } from "./ManagerRoutes";
+
+const isLoading = true;
 
 export function Routes() {
+	if (isLoading) {
+		return <Loading />;
+	}
 	return (
 		<BrowserRouter>
-			<ManagerRoutes />
+			<AuthRoutes />
 		</BrowserRouter>
 	);
 }
