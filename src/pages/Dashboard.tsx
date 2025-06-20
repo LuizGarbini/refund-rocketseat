d053694ce785e677a971d4a35501192bea0e7974
LuizGarbini <garbasneto@gmail.com>
2025-06-20 14:38:38 -0300

    Componente de Pagination

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,6 +6,7 @@ import { formatCurrency } from "../utils/formatCurrency";
 
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { Pagination } from "../components/Pagionation";
 import { RefundItem } from "../components/RefundItem";
 
 const REFUND_EXAMPLE = {
@@ -46,6 +47,8 @@ export function Dashboard() {
 			<div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
 				<RefundItem data={REFUND_EXAMPLE} />
 			</div>
+
+			<Pagination current={1} total={10} />
 		</div>
 	);
 }
