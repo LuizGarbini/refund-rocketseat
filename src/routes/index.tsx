67e7d0f999891a45ec18fea27ec7ee72442cb397
LuizGarbini <garbasneto@gmail.com>
2025-06-20 16:04:26 -0300

    Role do Usuário

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,37 @@
 import { BrowserRouter } from "react-router";
 
 import { Loading } from "../components/Loading";
-//import { EmployeeRoutes } from "./EmployeeRoutes";
 import { AuthRoutes } from "./AuthRoutes";
-//import { ManagerRoutes } from "./ManagerRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 
-const isLoading = true;
+const isLoading = false;
+//const session = undefined;
+
+const session = {
+	user: {
+		role: "employee",
+	},
+};
 
 export function Routes() {
+	function Route() {
+		switch (session?.user.role) {
+			case "empolyee":
+				return <EmployeeRoutes />;
+			case "manager":
+				return <ManagerRoutes />;
+			default:
+				return <AuthRoutes />;
+		}
+	}
+
 	if (isLoading) {
 		return <Loading />;
 	}
 	return (
 		<BrowserRouter>
-			<AuthRoutes />
+			<Route />
 		</BrowserRouter>
 	);
 }
