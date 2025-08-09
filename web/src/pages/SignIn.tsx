637b9b141a85bae71e54646e086969e7dee6996b
LuizGarbini <garbasneto@gmail.com>
2025-08-08 21:00:55 -0300

    Utilizando Action para puxar os valores do formulário

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -3,32 +3,28 @@ import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
 
-	function onSubmit(e: React.FormEvent) {
-		e.preventDefault();
-
-		console.log(email, password);
+	function onSignin(formData: FormData) {
+		console.log(formData.get("password"));
 	}
 
 	return (
-		<form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+		<form action={onSignin} className="w-full flex flex-col gap-4">
 			<Input
+				name="email"
 				required
 				legend="E-mail"
 				type="email"
 				placeholder="seu@email.com"
-				onChange={(e) => setEmail(e.target.value)}
 			/>
 
 			<Input
+				name="password"
 				required
 				legend="Senha"
 				type="password"
 				placeholder="123456"
-				onChange={(e) => setPassword(e.target.value)}
 			/>
 
 			<Button type="submit" isLoading={isLoading}>
