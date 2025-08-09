12e1f83bd6f735a40b6aebb77fe9eac57d365654
LuizGarbini <garbasneto@gmail.com>
2025-08-09 11:07:12 -0300

    Contexto em detalhes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter } from "react-router";
 
 import { Loading } from "../components/Loading";
+
+import { useAuth } from "../hooks/useAuth";
+
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
@@ -15,6 +18,9 @@ const session = {
 };
 
 export function Routes() {
+	const context = useAuth();
+	console.log(context);
+
 	function Route() {
 		switch (session?.user.role) {
 			case "employee":
